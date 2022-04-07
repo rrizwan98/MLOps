24cda85e9a5339a539c9814fb1defcb8d0b77217
rrizwan98 <rrizwan1998@gmail.com>
2022-04-07 15:31:39 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fist_mlops_project",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="Raza Rizwan", 
+    packages=find_packages(),
+    license="MIT"
+)
