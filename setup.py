d3d82112a7d96c06656b0330426135695b3ea7c1
rrizwan98 <rrizwan1998@gmail.com>
2022-04-11 15:31:22 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fist_mlops_project",
+    name="src",
     version="0.0.1",
     description="its a wine Q package", 
     author="Raza Rizwan", 
-    packages=find_packages(),
-    license="MIT"
+    packages=find_packages()
 )
